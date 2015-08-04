8498ec5a8183132a94089d7ca0ae98a91e576d56
exabyte0815 <exabyte0815@googlemail.com>
2015-08-04 14:26:12 +0200

    Update alu.vhd

--- src/vhdl/alu.vhd
+++ src/vhdl/alu.vhd
@@ -4,9 +4,10 @@
 
 library IEEE;
   use IEEE.std_logic_1164.ALL;
+  USE IEEE.numeric_std.ALL;
 
-library WORK;
-  use WORK.cpu_pack.all;
+--library WORK;
+  --use WORK.cpu_pack.all;
 
 entity alu is
 
@@ -22,41 +23,33 @@ end entity alu;
 
 architecture behave of alu is
 begin
-  process(a, b, function_code)
+  process(in_a, in_b, function_code)
   -- declaration of variables
-  variable a_uns : unsigned(31 downto 0);
-  variable b_uns : unsigned(31 downto 0);
-  variable r_uns : unsigned(31 downto 0);
-  variable z_uns : unsigned(0 downto 0);
+  variable a_uns : std_logic_vector(31 downto 0);
+  variable b_uns : std_logic_vector(31 downto 0);
+  variable r_uns : std_logic_vector(31 downto 0);
+  variable z_uns : std_logic_vector(0 downto 0);
   begin
     -- initialize values
-    a_uns := unsigned(in_a);
-    b_uns := unsigned(in_b);
+    a_uns := in_a;
+    b_uns := in_b;
     r_uns := (others => '0');
-    z_uns := '0';
+    z_uns := b"10_0000";
     -- select operation
     case function_code is
     -- add
-    when MIPS_FUNC_ADD =>
-    r_uns := a_uns + b_uns;
+    when b"10_0000" =>
+    r_uns <= a_uns + b_uns;
     -- others
     when others => r_uns := (others => 'X');
     end case;
-    if to_integer(r_uns) = 0 then
-    z_uns := '1';
+    if r_uns = x"0000_0000" then
+    z_uns := b"1";
     else
-    z_uns := '0':
+    z_uns := b"0";
     end if;
     -- assign variables to output signals
     result <= r_uns;
     zero <= z_uns;
 end process;
 end architecture behave;
-
-
-
-architecture structure_alu of alu is
-
-begin
-
-end architecture structure_alu;
