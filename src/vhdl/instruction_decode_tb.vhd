0cc89700a1ddc28f1045b6bc8dc1bbef8a58c9a4
Lukas Jäger <crimson_sunrise@gmx.de>
2015-08-04 11:43:01 +0200

    Added decode test for an add-instruction to instruction_decode

--- src/vhdl/instruction_decode_tb.vhd
+++ src/vhdl/instruction_decode_tb.vhd
@@ -13,10 +13,11 @@ architecture behavioural of instruction_decode_tb is
     --  DUT
     component instruction_decode
     port(instr,ip_in, writeback, alu_result: in std_logic_vector (31 downto 0);
+        writeback_reg : in std_logic_vector (4 downto 0);
         regdest_mux, regshift_mux: in std_logic_vector (1 downto 0);
         clk, reset, enable_regs: in std_logic;
-        reg_a, reg_b, shift_out, ip_out : out std_logic_vector (31 downto 0);
-        reg_dest : out std_logic_vector (4 downto 0);
+        reg_a, reg_b : out std_logic_vector (31 downto 0);
+        reg_dest, shift_out : out std_logic_vector (4 downto 0);
         imm : out std_logic_vector (15 downto 0)
         );
     end component;
@@ -25,6 +26,7 @@ architecture behavioural of instruction_decode_tb is
     signal ip_in : std_logic_vector (31 downto 0) := x"00000000";
     signal writeback : std_logic_vector (31 downto 0) := x"00000000";
     signal alu_result : std_logic_vector (31 downto 0) := x"00000000";
+    signal writeback_reg : std_logic_vector (5 downto 0) := '00001';
     signal regdest_mux : std_logic_vector (1 downto 0) := '00';
     signal regshift_mux : std_logic_vector (1 downto 0) := '00';
     signal clk : std_logic := '0';
@@ -37,6 +39,7 @@ begin
         instr => instr, 
         ip_in => ip_in,
         writeback => writeback,
+        writeback_reg => writeback_reg,
         alu_result => alu_result,
         regdest_mux => regdest_mux,
         regshift_mux => regshift_mux,
@@ -50,6 +53,34 @@ begin
         wait for clk_time / 2;
         clk <= '1';
         wait for clk_time / 2;
-    end process;        
+    end process;
+    
+    data_proc : process
+    begin
+        enable_regs = '1';
+        -- Writing some test values to the register file:
+        -- r1 becomes 01234567
+        writeback_reg <= 00001;
+        writeback <= x"01234567";
+        wait for clk_time;
+        
+        -- r2 becomes 76543210
+        writeback_reg <= 00010;
+        writeback <= x"7654321";
+        wait for clk_time;
+        
+        -- Real testing starts here
+        enable_regs <= '0';
+        
+        --inserting an add-instruction that adds r1 and r2 to r3
+        --outputs should be (all vals in hex notation):
+        --  reg_a: 01234567
+        --  reg_b: 76543210
+        --  reg_dest: 3;
+        --  imm: 1820
+        --  shift: 0
+        instr <= x"00221820";
+        wait for clk_time;
+    end process;
 end;
             
