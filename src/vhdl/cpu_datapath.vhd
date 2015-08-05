9cd3514aad8512293c73611ae5aeac16eb580740
exabyte0815 <exabyte0815@googlemail.com>
2015-08-05 17:06:35 +0200

    Update cpu_datapath.vhd

--- src/vhdl/cpu_datapath.vhd
+++ src/vhdl/cpu_datapath.vhd
@@ -10,6 +10,28 @@ end entity cpu_datapath;
 
 architecture structure_cpu_datapath of cpu_datapath is
 
+  -- -------- Instr. Fetch ==> Instr. Decode -----------------
+  signal instr_1         : std_logic_vector(31 downto 0);
+  signal ip_1            : std_logic_vector(31 downto 0);
+  
+  -- -------- Instr. Decode ==> Execution -----------------
+  signal shift_2          : std_logic_vector(4 downto 0);
+  signal reg_a_2          : std_logic_vector(31 downto 0);
+  signal reg_b_2          : std_logic_vector(31 downto 0);
+  signal regdest_2        : std_logic_vector(4 downto 0);
+  signal imm_2            : std_logic_vector(31 downto 0);
+  signal ip_2             : std_logic_vector(31 downto 0);
+
+  -- -------- Execution ==> Memory Stage -----------------
+  signal alu_result_3     : std_logic_vector(31 downto 0);
+  signal data_3           : std_logic_vector(31 downto 0);
+  signal regdest_3        : std_logic_vector(4 downto 0);
+
+  -- -------- Memory Stage ==> Write Back -----------------
+  signal writeback_4      : std_logic_vector(31 downto 0);
+  signal regdest_4        : std_logic_vector(4 downto 0);
+
+
 begin
 
 instruction_fetch:    entity work.instruction_fetch(behavioral) port map(  TODO );
