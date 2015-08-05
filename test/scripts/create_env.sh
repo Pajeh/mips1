e3a22cbb305d83d833744a1ab9e33a9755814fce
bahrienis <bahrienisdemirtel09@hotmail.com>
2015-08-05 17:04:57 +0200

    Instruction Fetch and write back add to env file

--- test/scripts/create_env.sh
+++ test/scripts/create_env.sh
@@ -6,6 +6,8 @@ hdllab/test/scripts/create_design.pl -a cpu_full  	$HOME/hdllab/test/conf
 hdllab/test/scripts/create_design.pl -a lab       	$HOME/hdllab/test/conf
 hdllab/test/scripts/create_design.pl -a execution   $HOME/hdllab/test/conf
 hdllab/test/scripts/create_design.pl -a mem_stage   $HOME/hdllab/test/conf
+hdllab/test/scripts/create_design.pl -a instruction_fetch   $HOME/hdllab/test/conf
+hdllab/test/scripts/create_design.pl -a write_back   $HOME/hdllab/test/conf
 
 
 echo "alias sim \"create_design.pl -sim\"
