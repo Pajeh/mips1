1107eed0792328e495e7e61fe72ea841846193c8
exabyte0815 <exabyte0815@googlemail.com>
2015-08-05 16:31:39 +0200

    Update cpu_datapath.vhd

--- src/vhdl/cpu_datapath.vhd
+++ src/vhdl/cpu_datapath.vhd
@@ -1,5 +1,7 @@
 -- revision history:
 -- 06.07.2015     Alex Schönberger    created
+-- 05.08.2015     Patrick Appenheimer    first try
+
 
 entity cpu_datapath is
 
@@ -9,5 +11,12 @@ end entity cpu_datapath;
 architecture structure_cpu_datapath of cpu_datapath is
 
 begin
+
+instruction_fetch:    entity work.instruction_fetch(behavioral) port map(  TODO );
+instruction_decode:   entity work.instruction_decode(behavioral) port map(  TODO );
+execution:            entity work.execution(behave) port map(  TODO );
+memory_stage:         entity work.MemoryStage(behavioral) port map(  TODO );
+write_back:           entity work.write_back(behavioral) port map(  TODO );
+
 
 end architecture structure_cpu_datapath;
