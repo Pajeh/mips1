da5dfd6abf81bcb6f283eaf73292456f5bb86011
exabyte0815 <exabyte0815@googlemail.com>
2015-08-06 10:11:43 +0200

    Update cpu_datapath.vhd

--- src/vhdl/cpu_datapath.vhd
+++ src/vhdl/cpu_datapath.vhd
@@ -16,7 +16,8 @@ entity cpu_datapath is
       alu_op                : in  std_logic_vector(5 downto 0);
       exc_mux1              : in  std_logic_vector(1 downto 0);
       exc_mux2              : in  std_logic_vector(1 downto 0);
-      exc_alu_zero          : out std_logic_vector(0 downto 0)
+      exc_alu_zero          : out std_logic_vector(0 downto 0);
+      memstg_mux            : in  std_logic
       
     );
 
@@ -56,15 +57,19 @@ instruction_fetch:    entity work.instruction_fetch(behavioral) port map(  TODO 
 instruction_decode:   entity work.instruction_decode(behavioral) port map(  TODO );
 
 -- EXECUTION:
-variable alu_result   : std_logic_vector(31 downto 0);
-variable data_out     : std_logic_vector(31 downto 0);
-variable destreg_out  : std_logic_vector(4  downto 0);
+variable alu_result       : std_logic_vector(31 downto 0);
+variable data_out         : std_logic_vector(31 downto 0);
+variable exc_destreg_out  : std_logic_vector(4  downto 0);
 execution:            entity work.execution(behave) port map(clk, rst, alu_result, data_out, destreg_out,
                                                               exc_alu_zero, reg_a_2, reg_b_2, regdest_2, imm_2,
                                                               ip_2, shift_2, exc_mux1, exc_mux2,alu_op);
 
 -- MEMORY STAGE:
-memory_stage:         entity work.MemoryStage(behavioral) port map(  TODO );
+variable writeback            : std_logic_vector(31 downto 0);
+variable memstg_destreg_out   : std_logic_vector(4  downto 0);
+memory_stage:         entity work.MemoryStage(behavioral) port map(clk, rst, alu_result_3, data_3, data_addr,
+                                                                    data_from_cpu, data_to_cpu, memstg_mux,
+                                                                    writeback, regdest_3, memstg_destreg_out);
 
 -- WRITE BACK:
 write_back:           entity work.write_back(behavioral) port map(  TODO );
@@ -75,10 +80,14 @@ begin
     alu_result_3 <= (others => '0');
     data_3 <= (others => '0');
     regdest_3 <= (others => '0');
+    writeback_4 <= (others => '0');
+    regdest_4 <= (others => '0');
   elsif (rising_edge(clk)) then
     alu_result_3 <= alu_result;
     data_3 <= data_out;
     regdest_3 <= destreg_out;
+    writeback_4 <= writeback;
+    regdest_4 <= memstg_destreg_out;
   end if;
 end process;
 
