296602496279896eaa74240d0fc775407b45ff9f
exabyte0815 <exabyte0815@googlemail.com>
2015-08-06 13:23:55 +0200

    Update cpu_datapath.vhd

--- src/vhdl/cpu_datapath.vhd
+++ src/vhdl/cpu_datapath.vhd
@@ -60,59 +60,53 @@ architecture structure_cpu_datapath of cpu_datapath is
   signal regdest_4        : std_logic_vector(4 downto 0);
   
 
-signal if_ip      : std_logic_vector(31 downto 0);
-signal if_instr   : std_logic_vector(31 downto 0);
+  signal if_ip      : std_logic_vector(31 downto 0);
+  signal if_instr   : std_logic_vector(31 downto 0);
 
-signal id_a       : std_logic_vector(31 downto 0);
-signal id_b       : std_logic_vector(31 downto 0);
-signal id_imm     : std_logic_vector(31 downto 0);
-signal id_ip      : std_logic_vector(31 downto 0);
-signal id_regdest : std_logic_vector(4 downto 0);
-signal id_shift   : std_logic_vector(4 downto 0);
+  signal id_a       : std_logic_vector(31 downto 0);
+  signal id_b       : std_logic_vector(31 downto 0);
+  signal id_imm     : std_logic_vector(31 downto 0);
+  signal id_ip      : std_logic_vector(31 downto 0);
+  signal id_regdest : std_logic_vector(4 downto 0);
+  signal id_shift   : std_logic_vector(4 downto 0);
 
-signal alu_result       : std_logic_vector(31 downto 0);
-signal data_out         : std_logic_vector(31 downto 0);
-signal exc_destreg_out  : std_logic_vector(4  downto 0);
+  signal alu_result       : std_logic_vector(31 downto 0);
+  signal data_out         : std_logic_vector(31 downto 0);
+  signal exc_destreg_out  : std_logic_vector(4  downto 0);
 
-signal memstg_writeback_out : std_logic_vector(31 downto 0);
-signal memstg_destreg_out   : std_logic_vector(4  downto 0);
+  signal memstg_writeback_out : std_logic_vector(31 downto 0);
+  signal memstg_destreg_out   : std_logic_vector(4  downto 0);
 
-signal wb_writeback_out : std_logic_vector(31 downto 0);
-signal wb_destreg_out   : std_logic_vector(4  downto 0);
+  signal wb_writeback_out : std_logic_vector(31 downto 0);
+  signal wb_destreg_out   : std_logic_vector(4  downto 0);
 
-signal mux_pc_out      : std_logic_vector(31 downto 0);
+  signal mux_pc_out      : std_logic_vector(31 downto 0);
 
 begin
 
 -- INSTRUCTION FETCH:
-
-
-instruction_fetch:    entity work.instruction_fetch(behavioral) port map(clk, rst, mux_out_0, instr_in, if_ip,
+  instruction_fetch:    entity work.instruction_fetch(behavioral) port map(clk, rst, mux_out_0, instr_in, if_ip,
                                                                           instr_addr, if_instr);
 
 -- INSTRUCTION DECODE:
-
-instruction_decode:   entity work.instruction_decode(behavioral) port map(instr_1, ip_1, wb_writeback_out, alu_result,
+  instruction_decode:   entity work.instruction_decode(behavioral) port map(instr_1, ip_1, wb_writeback_out, alu_result,
                                                                           memstg_writeback_out, exc_destreg_out,
                                                                           memstg_destreg_out, id_regdest_mux,
                                                                           id_regshift_mux, clk, rst, id_enable_regs,
                                                                           id_a, id_b, id_imm, id_ip, id_regdest, id_shift);
 
 -- EXECUTION:
-
-execution:            entity work.execution(behave) port map(clk, rst, alu_result, data_out, exc_destreg_out,
+  execution:            entity work.execution(behave) port map(clk, rst, alu_result, data_out, exc_destreg_out,
                                                               exc_alu_zero, reg_a_2, reg_b_2, regdest_2, imm_2,
                                                               ip_2, shift_2, exc_mux1, exc_mux2,alu_op);
 
 -- MEMORY STAGE:
-
-memory_stage:         entity work.MemoryStage(behavioral) port map(clk, rst, alu_result_3, data_3, data_addr,
+  memory_stage:         entity work.MemoryStage(behavioral) port map(clk, rst, alu_result_3, data_3, data_addr,
                                                                     data_from_cpu, data_to_cpu, memstg_mux,
                                                                     memstg_writeback_out, regdest_3, memstg_destreg_out);
 
 -- WRITE BACK:
-
-write_back:           entity work.write_back(behavioral) port map(clk, rst, writeback_4, regdest_4,
+  write_back:           entity work.write_back(behavioral) port map(clk, rst, writeback_4, regdest_4,
                                                                   wb_writeback_out, wb_destreg_out);
 
 path: process(clk, rst)
