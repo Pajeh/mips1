a41aaa22e6eb64df2a61417a81e0613d3c8e7096
Lukas Jäger <crimson_sunrise@gmx.de>
2015-08-06 10:48:49 +0200

    Fixed JAL-bug in instruction decode

--- src/vhdl/instruction_decode.vhd
+++ src/vhdl/instruction_decode.vhd
@@ -4,6 +4,7 @@
 -- 2015-08-04   Lukas Jäger     added asynchronous reset
 -- 2015-08-05	Lukas Jaeger	 fixed bugs that resulted from me not knowing any VHDL
 -- 2015-08-05	Lukas Jaeger	 added functionality for branch logic
+-- 2015-08-06	Lukas, Carlos 	 fixed bug in JAL-instruction-decode
 library IEEE;
     use IEEE.std_logic_1164.all;
 	use IEEE.numeric_std.all;
@@ -31,7 +32,7 @@ begin
 	imm <= imm_internal;        -- Imm is the subvector of instr from 15 to 0 and it is padded with leading zeros for further processing.
  	-- Splitting registers for R-type-instructions
     	pc_imm <= imm_internal (31 downto 2) & "00";
-	internal_writeback <= x"00000000";
+	--internal_writeback <= x"00000000";
 	-- Defines the instruction decode logic
     	logic : process (instr, ip_in, writeback, alu_result, writeback_reg, regdest_ex, regdest_mem, regdest_mux, regshift_mux)  is
     		begin
@@ -95,10 +96,12 @@ begin
 			offset := to_integer(signed(instr(25 downto 0)));
 			offset := offset * 4;
 			ip_out <= ip_in (31 downto 28) & std_logic_vector(to_signed(offset,28));
-		elsif (instr (31 downto 26) = "000011") then --JAL instruction
+		elsif ((instr (31 downto 26) = "000011") and (reset = '1'))then --JAL instruction
 			-- TODO: Fix that bloody bug
 			internal_writeback <= std_logic_vector(to_unsigned(to_integer(unsigned(ip_in)) + 4,32));
 			internal_wb_flag <= '1';
+			offset := to_integer(signed(instr(25 downto 0)));
+			offset := offset * 4;
 			ip_out <= ip_in (31 downto 28) & std_logic_vector(to_signed(offset,28));
 		elsif ((instr(31 downto 26) = "000000") and (instr (20 downto 0) ="000000000000000001000")) then --JR instruction
 			internal_wb_flag <= '0';
