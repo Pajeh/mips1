c7cf50c846b70a183e532a934add4eb584f89862
exabyte0815 <exabyte0815@googlemail.com>
2015-08-07 11:40:13 +0200

    Update cpu.vhd

--- src/vhdl/cpu.vhd
+++ src/vhdl/cpu.vhd
@@ -25,6 +25,17 @@ end entity cpu;
 
 architecture structure_cpu of cpu is
 
+signal alu_op               : std_logic_vector(5 downto 0);
+signal exc_mux1             : std_logic_vector(1 downto 0);
+signal exc_mux2             : std_logic_vector(1 downto 0);
+signal exc_alu_zero         : std_logic_vector(0 downto 0);
+signal memstg_mux           : std_logic;
+signal id_regdest_mux       : std_logic_vector (1 downto 0);
+signal id_regshift_mux      : std_logic_vector (1 downto 0);
+signal id_enable_regs       : std_logic;
+signal in_mux_pc            : std_logic;
+signal stage_control        : std_logic_vector (4 downto 0):
+
 begin
 
   --
@@ -35,12 +46,11 @@ begin
   --   PORT MAP( 
   --   );
 
-  --
+  
   -- datapath
-  --
-  -- u2_datapath: cpu_datapath
-  --   -- GENERIC MAP( )
-  --   PORT MAP(
-  --   );
+   u2_datapath: cpu_datapath
+     PORT MAP(clk, rst, instr_addr, data_addr, instr_in, data_to_cpu, data_from_cpu, alu_op, exc_mux1, exc_mux2,
+              exc_alu_zero, memstg_mux, id_regdest_mux, id_regshift_mux, id_enable_regs, in_mux_pc, stage_control
+     );
 
 end architecture structure_cpu;
