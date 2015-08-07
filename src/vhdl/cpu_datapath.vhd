be2d8e9f78999c647dfbe288f623f3adc6e07930
Lukas Jäger <crimson_sunrise@gmx.de>
2015-08-07 09:29:03 +0200

    Replaced a case statement in cpu_datapath

--- src/vhdl/cpu_datapath.vhd
+++ src/vhdl/cpu_datapath.vhd
@@ -184,16 +184,21 @@ end process;
 
 mux: process(in_mux_pc)
   begin
-    case in_mux_pc is
+    --case in_mux_pc is
       -- 0
-      when '0' =>
-      mux_pc_out <= ip_1;
+      --when '0' =>
+      --mux_pc_out <= ip_1;
       -- 1
-      when '1' =>
-      mux_pc_out <= id_ip;
+      --when '1' =>
+      --mux_pc_out <= id_ip;
       -- others
-      when others => mux_pc_out <= (others => 'X');
-    end case;
+      --when others => mux_pc_out <= (others => 'X');
+    --end case;
+	if (in_mux_pc = '1') then
+		mux_pc_out <= id_ip;
+	else
+		mux_pc_out <= ip_1;
+	end if;
   end process;
 
 
