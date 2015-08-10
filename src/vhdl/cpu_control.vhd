5daa325b12507675478ae9ad33f4f72d7b650cc0
exabyte0815 <exabyte0815@googlemail.com>
2015-08-10 16:11:00 +0200

    Update cpu_control.vhd

--- src/vhdl/cpu_control.vhd
+++ src/vhdl/cpu_control.vhd
@@ -38,8 +38,28 @@ end entity cpu_control;
 
 architecture structure_cpu_control of cpu_control is
 
+  signal instr1         : std_logic_vector(31 downto 0);
+  signal instr2         : std_logic_vector(31 downto 0);
+  signal instr3         : std_logic_vector(31 downto 0);
+  signal instr4         : std_logic_vector(31 downto 0);
+  signal instr5         : std_logic_vector(31 downto 0);
+
+  signal state1         : std_logic_vector(4 downto 0);
+  signal state2         : std_logic_vector(4 downto 0);
+  signal state3         : std_logic_vector(4 downto 0);
+  signal state4         : std_logic_vector(4 downto 0);
+  signal state5         : std_logic_vector(4 downto 0);
+  
+  signal finished1      : std_logic;
+
+  
 begin
 
+	fsm1:	entity work.fsm(behavioral) port map(clk, rst, instr1, state1, state5, state2, finished1);
+	fsm2:	entity work.fsm(behavioral) port map(clk, rst, instr2, state2, state1, state3, finished2);
+	fsm3:	entity work.fsm(behavioral) port map(clk, rst, instr3, state3, state2, state4, finished3);
+	fsm4:	entity work.fsm(behavioral) port map(clk, rst, instr4, state4, state3, state5, finished4);
+	fsm5:	entity work.fsm(behavioral) port map(clk, rst, instr5, state5, state4, state1, finished5);
 
 	
 end architecture structure_cpu_control;
