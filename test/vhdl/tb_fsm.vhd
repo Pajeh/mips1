44ef2247ead1d49ca05c0c2d5e115931b63f3bec
Carlos Minamisava Faria <faria.ee@gmail.com>
2015-08-11 14:39:02 +0200

    minor report updates

--- test/vhdl/tb_fsm.vhd
+++ test/vhdl/tb_fsm.vhd
@@ -21,21 +21,16 @@ architecture behav_tb_fsm of tb_fsm is
   -- -------- FSM INTERFACE -----------------
   signal clk                : std_logic := '0';
   signal rst                : std_logic;
-  signal test_pc_mux        : std_logic;
-  signal test_instr         : std_logic_vector(31 downto 0);
-  signal test_regdest_mux   : std_logic_vector(1 downto 0);
-  signal test_regshift_mux  : std_logic_vector(1 downto 0);
-  signal test_enable_regs   : std_logic;
-  signal test_in_mux1       : std_logic_vector(1 downto 0);
-  signal test_in_mux2       : std_logic_vector(1 downto 0);
-  signal test_alu_instr     : std_logic_vector(5 downto 0);
-  signal test_zero          : std_logic_vector(0 downto 0);
-  signal test_memstg_mux    : std_logic;
-  signal test_rd_mask       : std_logic_vector(3 downto 0);
-  signal test_wr_mask       : std_logic_vector(3 downto 0);
+signal       instr_in : std_logic_vector(31 downto 0);
+signal fsm_busy: std_logic;
+signal       nextfsm_busy: std_logic;
+signal       nextfsm_state: std_logic_vector(4 downto 0);
+signal       output_buffer : std_logic_vector(29 downto 0);
+
+    signal instr  :   std_logic_vector(31 downto 0); 
+  
   signal test_instr_stall   : std_logic;
   signal test_data_stall    : std_logic;
-  signal test_stage_control : std_logic_vector(4 downto 0);
 
   -- ------ SIMULATION CONTROL ---------------
   signal sim_finish : std_logic;
@@ -51,8 +46,7 @@ begin
 
   -- FSM
   u1_fsm : entity work.fsm(behavioral)
-    port map(clk, rst, test_pc_mux, test_instr, test_regdest_mux, test_regshift_mux, test_enable_regs, test_in_mux1, test_in_mux2,
-             test_alu_instr, test_zero, test_memstg_mux, test_rd_mask, test_wr_mask, test_instr_stall, test_data_stall, test_stage_control);
+    port map(clk, rst, fsm_busy, nextfsm_busy, nextfsm_state, output_buffer, instr_in, test_instr_stall, test_data_stall);
 
 
   -- TEST PROCESS
