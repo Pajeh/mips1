36788f40b193dff8b313f854d582affa27b2b881
Carlos Minamisava Faria <faria.ee@gmail.com>
2015-08-12 10:46:08 +0200

    compilable

--- src/vhdl/cpu_control.vhd
+++ src/vhdl/cpu_control.vhd
@@ -7,6 +7,7 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
+use IEEE.std_logic_misc.all;
 
 library WORK;
 use WORK.all;
@@ -45,32 +46,34 @@ architecture structure_cpu_control of cpu_control is
   constant s4 : std_logic_vector(4 downto 0) := b"00100";
 
   type t_states is array (4 downto 0) of std_logic_vector(4 downto 0);
-  signal currentstate  : t_states := ((others  => (others => '0')));
-  signal nextstate     : t_states  := ((others  => (others => '0')));
-  signal output_buffer : t_states := ((others  => (others => '0')));
-  signal rst_fsm       : std_logic_vector(4 downto 0) := (others                     => '1');
+  signal currentstate : t_states := ((others => (others => '0')));
+  signal nextstate    : t_states := ((others => (others => '0')));
+
+  type t_output is array (4 downto 0) of std_logic_vector(29 downto 0);
+  signal output_buffer : t_output                     := ((others => (others => '0')));
+  signal rst_fsm       : std_logic_vector(4 downto 0) := (others  => '1');
   
 begin
-  fsm1 : entity work.fsm(behavioral) port map(clk, rst_fsm(0), nextstate(4), nextstate(0), currentstate(0), instr_in, instr_stall, data_stall);
-  fsm2 : entity work.fsm(behavioral) port map(clk, rst_fsm(1), nextstate(0), nextstate(1), currentstate(1), instr_in, instr_stall, data_stall);
-  fsm3 : entity work.fsm(behavioral) port map(clk, rst_fsm(2), nextstate(1), nextstate(2), currentstate(2), instr_in, instr_stall, data_stall);
-  fsm4 : entity work.fsm(behavioral) port map(clk, rst_fsm(3), nextstate(2), nextstate(3), currentstate(3), instr_in, instr_stall, data_stall);
-  fsm5 : entity work.fsm(behavioral) port map(clk, rst_fsm(4), nextstate(3), nextstate(4), currentstate(4), instr_in, instr_stall, data_stall);
+  fsm1 : entity work.fsm(behavioral) port map(clk, rst_fsm(0), nextstate(4), nextstate(0), currentstate(0), output_buffer(0), instr_in, instr_stall, data_stall);
+  fsm2 : entity work.fsm(behavioral) port map(clk, rst_fsm(1), nextstate(0), nextstate(1), currentstate(1), output_buffer(1), instr_in, instr_stall, data_stall);
+  fsm3 : entity work.fsm(behavioral) port map(clk, rst_fsm(2), nextstate(1), nextstate(2), currentstate(2), output_buffer(2), instr_in, instr_stall, data_stall);
+  fsm4 : entity work.fsm(behavioral) port map(clk, rst_fsm(3), nextstate(2), nextstate(3), currentstate(3), output_buffer(3), instr_in, instr_stall, data_stall);
+  fsm5 : entity work.fsm(behavioral) port map(clk, rst_fsm(4), nextstate(3), nextstate(4), currentstate(4), output_buffer(4), instr_in, instr_stall, data_stall);
 
 
-  init : process(rst, nextstate)
+  init : process(rst, currentstate)
   begin
     if rst'event and rst = '0' then     -- reset condition forwarded
-      rst_fsm(4 downto 0) <= others => '0';
+      rst_fsm(4 downto 0) <= (others => '1');
     else
-      -- if all nextstates are zero, we are at the initial condition
-      if (nextstate(4 downto 0, 0) nor nextstate(4 downto 0, 1) nor nextstate(4 downto 0, 2) nor nextstate(4 downto 0, 3) nor nextstate(4 downto 0, 4)) = b"00000" then
+      -- if all currentstates are zero, we are at the initial condition
+      if currentstate(0) = s0 and currentstate(1) = s0 and currentstate(2) = s0 and currentstate(3) = s0 and currentstate(4) = s0 then
         rst_fsm(4 downto 0) <= b"00001";
-      elsif (nextstate(4 downto 0, 1) nor nextstate(4 downto 0, 2) nor nextstate(4 downto 0, 3) nor nextstate(4 downto 0, 4)) = b"00000" then
+      elsif currentstate(1) = s0 and currentstate(2) = s0 and currentstate(3) = s0 and currentstate(4) = s0 then
         rst_fsm(4 downto 0) <= b"00011";
-      elsif (nextstate(4 downto 0, 2) nor nextstate(4 downto 0, 3) nor nextstate(4 downto 0, 4)) = b"00000" then
+      elsif currentstate(2) = s0 and currentstate(3) = s0 and currentstate(4) = s0 then
         rst_fsm(4 downto 0) <= b"00111";
-      elsif (nextstate(4 downto 0, 3) nor nextstate(4 downto 0, 4)) = b"00000" then
+      elsif currentstate(3) = s0 and currentstate(4) = s0 then
         rst_fsm(4 downto 0) <= b"01111";
       else
         rst_fsm(4 downto 0) <= b"11111";
@@ -78,30 +81,120 @@ begin
     end if;
   end process init;
 
-  outputs : process(output_buffer)
+  output_s0 : process(output_buffer, clk, currentstate, nextstate)
   begin
-    case currentstate is
-      when s0 =>
-        id_regdest_mux  <= output_buffer1 (28 downto 27);
-        id_regshift_mux <= output_buffer1 (26 downto 25);
-      when s1 =>
-        exc_mux1          <= output_buffer1 (23 downto 22);
-        exc_mux2          <= output_buffer1 (21 downto 20);
-        alu_op            <= output_buffer1 (19 downto 14);
-        in_mux_pc         <= output_buffer1 (29);
-        stage_control (2) <= output_buffer1 (2);
-      when s2 =>
-        memstg_mux        <= output_buffer1 (13);
-        rd_mask           <= output_buffer1 (12 downto 9);
-        wr_mask           <= output_buffer1 (8 downto 5);
-        stage_control (3) <= output_buffer1 (3);
-      when s3 =>
-        stage_control (4) <= output_buffer1 (4);
-      when s4 =>
-        id_enable_regs <= output_buffer1 (24);
-      when others =>
-    --do nothing
-    end case;
-  end process fsm1_ctrl;
-  
+    if (currentstate(0) = s0) then
+      id_regdest_mux  <= output_buffer(0) (28 downto 27);
+      id_regshift_mux <= output_buffer(0) (26 downto 25);
+    elsif (currentstate(1) = s1) then
+      id_regdest_mux  <= output_buffer(1) (28 downto 27);
+      id_regshift_mux <= output_buffer(1) (26 downto 25);
+    elsif (currentstate(2) = s2) then
+      id_regdest_mux  <= output_buffer(2) (28 downto 27);
+      id_regshift_mux <= output_buffer(2) (26 downto 25);
+    elsif (currentstate(3) = s3) then
+      id_regdest_mux  <= output_buffer(3) (28 downto 27);
+      id_regshift_mux <= output_buffer(3) (26 downto 25);
+    elsif (currentstate(4) = s4) then
+      id_regdest_mux  <= output_buffer(4) (28 downto 27);
+      id_regshift_mux <= output_buffer(4) (26 downto 25);
+    end if;
+  end process output_s0;
+
+  output_s1 : process(output_buffer, clk, currentstate, nextstate)
+  begin
+    if (currentstate(0) = s0) then
+      exc_mux1          <= output_buffer(0) (23 downto 22);
+      exc_mux2          <= output_buffer(0) (21 downto 20);
+      alu_op            <= output_buffer(0) (19 downto 14);
+      in_mux_pc         <= output_buffer(0) (29);
+      stage_control (2) <= output_buffer(0) (2);
+    elsif (currentstate(1) = s1) then
+      exc_mux1          <= output_buffer(1) (23 downto 22);
+      exc_mux2          <= output_buffer(1) (21 downto 20);
+      alu_op            <= output_buffer(1) (19 downto 14);
+      in_mux_pc         <= output_buffer(1) (29);
+      stage_control (2) <= output_buffer(1) (2);
+    elsif (currentstate(2) = s2) then
+      exc_mux1          <= output_buffer(2) (23 downto 22);
+      exc_mux2          <= output_buffer(2) (21 downto 20);
+      alu_op            <= output_buffer(2) (19 downto 14);
+      in_mux_pc         <= output_buffer(2) (29);
+      stage_control (3) <= output_buffer(2) (2);
+    elsif (currentstate(3) = s3) then
+      exc_mux1          <= output_buffer(3) (23 downto 22);
+      exc_mux2          <= output_buffer(3) (21 downto 20);
+      alu_op            <= output_buffer(3) (19 downto 14);
+      in_mux_pc         <= output_buffer(3) (29);
+      stage_control (2) <= output_buffer(3) (2);
+    elsif (currentstate(4) = s4) then
+      exc_mux1          <= output_buffer(4) (23 downto 22);
+      exc_mux2          <= output_buffer(4) (21 downto 20);
+      alu_op            <= output_buffer(4) (19 downto 14);
+      in_mux_pc         <= output_buffer(4) (29);
+      stage_control (2) <= output_buffer(4) (2);
+    end if;
+  end process output_s1;
+
+  output_s2 : process(output_buffer, clk, currentstate, nextstate)
+  begin
+    if (currentstate(0) = s0) then
+      memstg_mux        <= output_buffer(0) (13);
+      rd_mask           <= output_buffer(0) (12 downto 9);
+      wr_mask           <= output_buffer(0) (8 downto 5);
+      stage_control (3) <= output_buffer(0) (3);
+    elsif (currentstate(1) = s1) then
+      memstg_mux        <= output_buffer(1) (13);
+      rd_mask           <= output_buffer(1) (12 downto 9);
+      wr_mask           <= output_buffer(1) (8 downto 5);
+      stage_control (3) <= output_buffer(1) (3);
+    elsif (currentstate(2) = s2) then
+      memstg_mux        <= output_buffer(2) (13);
+      rd_mask           <= output_buffer(2) (12 downto 9);
+      wr_mask           <= output_buffer(2) (8 downto 5);
+      stage_control (3) <= output_buffer(2) (3);
+    elsif (currentstate(3) = s3) then
+      memstg_mux        <= output_buffer(3) (13);
+      rd_mask           <= output_buffer(3) (12 downto 9);
+      wr_mask           <= output_buffer(3) (8 downto 5);
+      stage_control (3) <= output_buffer(3) (3);
+    elsif (currentstate(4) = s4) then
+      memstg_mux        <= output_buffer(4) (13);
+      rd_mask           <= output_buffer(4) (12 downto 9);
+      wr_mask           <= output_buffer(4) (8 downto 5);
+      stage_control (3) <= output_buffer(4) (3);
+    end if;
+  end process output_s2;
+
+  output_s3 : process(output_buffer, clk, currentstate, nextstate)
+  begin
+    if (currentstate(0) = s0) then
+      stage_control (4) <= output_buffer(0) (4);
+    elsif (currentstate(1) = s1) then
+      stage_control (4) <= output_buffer(1) (4);
+     elsif (currentstate(2) = s2) then
+      stage_control (4) <= output_buffer(2) (4);
+     elsif (currentstate(3) = s3) then
+      stage_control (4) <= output_buffer(3) (4);
+     elsif (currentstate(4) = s4) then
+      stage_control (4) <= output_buffer(4) (4);
+     end if;
+  end process output_s3;
+
+  output_s4 : process(output_buffer, clk, currentstate, nextstate)
+  begin
+    if (currentstate(0) = s0) then
+      id_enable_regs <= output_buffer(0) (24);
+    elsif (currentstate(1) = s1) then
+      id_enable_regs <= output_buffer(1) (24);
+    elsif (currentstate(2) = s2) then
+      id_enable_regs <= output_buffer(2) (24);
+    elsif (currentstate(3) = s3) then
+      id_enable_regs <= output_buffer(3) (24);
+    elsif (currentstate(4) = s4) then
+      id_enable_regs <= output_buffer(4) (24);
+    end if;
+  end process output_s4;
+
+
 end architecture structure_cpu_control;
