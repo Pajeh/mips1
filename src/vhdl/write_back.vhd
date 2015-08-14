0e670b86f411b9f9b0170f1aba248ff7f32dfa8d
exabyte0815 <exabyte0815@googlemail.com>
2015-08-14 15:57:29 +0200

    Update write_back.vhd

--- src/vhdl/write_back.vhd
+++ src/vhdl/write_back.vhd
@@ -1,43 +1,33 @@
 -- 03.08.2015     Bahri Enis Demirtel    created
-
+-- 14.08.2015     Patrick Appenheimer	 format
 
 library IEEE;
   use IEEE.std_logic_1164.ALL;
-
+  
 
 entity write_back is
-port(
-
-clk : in std_logic; 
-rst : in std_logic;
-writeback_in : in  std_logic_vector(31 downto 0);	-- : in  std_logic_vector(CPU_ADDR_WIDTH-1 downto 0);
-regdest_in : in  std_logic_vector(4 downto 0);		-- : in  std_logic_vector(CPU_REG_ADDR_WIDTH-1 downto 0);
-writeback_out : out std_logic_vector(31 downto 0);  -- :out  std_logic_vector(CPU_ADDR_WIDTH-1 downto 0);
-regdest_out : out std_logic_vector(4 downto 0)		-- : out std_logic_vector(CPU_REG_ADDR_WIDTH-1 downto 0)
-);
-
-
+	port(
+	clk		: in std_logic; 
+	rst		: in std_logic;
+	writeback_in	: in  std_logic_vector(31 downto 0);
+	regdest_in	: in  std_logic_vector(4 downto 0);
+	writeback_out	: out std_logic_vector(31 downto 0); 
+	regdest_out	: out std_logic_vector(4 downto 0)
+	);
 end entity write_back;
 
 
 architecture behavioral of write_back is
-
 begin
 
-process (rst, clk, writeback_in, regdest_in) is
+	process (rst, clk, writeback_in, regdest_in) is
 	begin
-
 		if(rst = '1') then
-		writeback_out <= x"0000_0000";		
-		regdest_out <= b"00000";
-		
-		
-		
+			writeback_out <= x"0000_0000";		
+			regdest_out <= b"00000";
 		else
-
 			writeback_out <= writeback_in;
 			regdest_out <= regdest_in;
-
 		end if;
 	end process ; 
 end architecture behavioral;
