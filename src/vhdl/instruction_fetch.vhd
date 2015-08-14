0a35133ef012aedddbc648404e3a2baa7fda5e30
exabyte0815 <exabyte0815@googlemail.com>
2015-08-14 15:54:37 +0200

    Update instruction_fetch.vhd

--- src/vhdl/instruction_fetch.vhd
+++ src/vhdl/instruction_fetch.vhd
@@ -1,4 +1,5 @@
 -- 03.08.2015     Bahri Enis Demirtel    created
+-- 14.08.2015     Patrick Appenheimer	 format
 
 
 library IEEE;
@@ -12,22 +13,15 @@ library WORK;
 
 
 entity instruction_fetch is
-port(
-
-clk : in std_logic;
-rst : in std_logic;
-
-PC : in std_logic_vector(31 downto 0);      --PC : in std_logic_vector(CPU_ADDR_WIDTH-1 downto 0);                        	--PC(32 bit)
-InstrData : in std_logic_vector(31 downto 0);			--InstrData : in std_logic_vector(CPU_DATA_WIDTH-1 downto 0);				 	    --InstrData, Adress information comes from Memory(32 bit)
-
---StallData : in std_logic;
-
-IR : out std_logic_vector(31 downto 0);    --IR : out std_logic_vector(CPU_ADDR_WIDTH-1 downto 0);   					    --IR, Next PC goes to Execution Stage(32 bit)
-InstrAddr: out std_logic_vector(31 downto 0); --InstrAddr: out std_logic_vector(CPU_ADDR_WIDTH-1 downto 0);  					--InstrAddr, PC goes to Memory(32 bit)
-Instr : out std_logic_vector(31 downto 0)	--Instr : out std_logic_vector(CPU_DATA_WIDTH-1 downto 0);				    	--Instr, Adress information from Memory goes to Instruction Decoder(32 bit)
-
-
-);
+	port(
+	clk : in std_logic;
+	rst : in std_logic;
+	PC : in std_logic_vector(31 downto 0);                             	
+	InstrData : in std_logic_vector(31 downto 0);
+	IP : out std_logic_vector(31 downto 0);	
+	InstrAddr: out std_logic_vector(31 downto 0);
+	Instr : out std_logic_vector(31 downto 0)
+	);
 
 end entity instruction_fetch;
 
@@ -35,29 +29,17 @@ end entity instruction_fetch;
 
 
 architecture behavioral of instruction_fetch is
-
-
-	begin
-
+begin
 	process (rst, clk, PC, InstrData) is
 	begin
-
 		if(rst = '1') then
-		InstrAddr <= X"0000_0000";	    --If reset comes PC goes to the beginning, its value is 0000_0000
-		IR <= X"0000_0000";				--If reset all coming signals are 0000_0000
-		Instr <= X"0000_0000";			--If reset all coming signals are 0000_0000
-		
-		
-			else
-						--elsif (rising_edge(clk)) then  
-						--if(StallData='0') then
-		
-		
-				InstrAddr <= PC;  		  --We can the value of PC to the memory adress
-				IR <= PC + X"0000_0004";  --IR value is always PC+4;
-				Instr <= InstrData;		  --Instr value is always equal to InstrData value
-				
-			
+			InstrAddr <= X"0000_0000";	--If reset comes PC goes to the beginning, its value is 0000_0000
+			IP <= X"0000_0000";		--If reset all coming signals are 0000_0000
+			Instr <= X"0000_0000";		--If reset all coming signals are 0000_0000
+		else
+			InstrAddr <= PC;  		--We can the value of PC to the memory adress
+			IP <= PC + X"0000_0004";	--IR value is always PC+4;
+			Instr <= InstrData;		--Instr value is always equal to InstrData value
 		end if;
 	end process ;
 
